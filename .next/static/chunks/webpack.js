1d2bff56810ade60b7a7113ba1c4b2167fef2dc1
JoMiras <jonah@bayvalleytech.com>
2023-09-18 00:02:42 -0700

    everything was working fine until I started to add the navbar stuff.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0be4fe732723681b"; }
+/******/ 		__webpack_require__.h = function() { return "6c47a2aee85fac24"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
