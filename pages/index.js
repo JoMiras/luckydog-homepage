67f51c381ab84ffc870c0ac89e5fafa83400742c
JoMiras <jonah@bayvalleytech.com>
2023-09-24 11:00:36 -0700

    .

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ const Page = () => {
     return (
         <Container>
             <Box borderRadius="lg" bg="red" p={3} mb={6} align="center" >
+                <div>
             Hello, I&apos;m a full-stack developer based in California!
+            </div>
                 </Box>
             <Box display={{md: 'flex'}}>
                 <Box flexGrow={1}>
