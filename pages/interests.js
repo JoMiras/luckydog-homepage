f7a3b1b8d99170dd1683e2518eb9987b9027c5aa
JoMiras <jonamiraso@gmail.com>
2024-08-24 23:06:42 -0700

    fix navbar, workign on proficiency page

--- pages/interests.js
+++ pages/interests.js
@@ -1,13 +1,39 @@
-import { Text, Container } from '@chakra-ui/react'
+import { Text, Container, Heading, SimpleGrid, Box, useColorModeValue } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
 
-const Test = () => {
-    return (
-        <Container mt= {{ base: '20px', md: '90px' }}> 
-        <Text><strong>
-            This page is under construction. Thank you for understanding.
-            </strong>
-            </Text>
-        </Container>
-    )
-}
-export default Test
+const proficiencies = [
+    { name: 'HTML & CSS '},
+    { name: 'JavaScript' },
+    { name: 'SQL' },
+    { name: 'Python'},
+    { name: 'Swift'},
+    { name: 'C++'}
+]
+
+const Proficiencies = () => (
+    <Layout title="Proficiencies"> 
+    <Container mt={{ base: '20px', md: '20px' }}>  
+        <Heading as="h3" fontSize={20} mb={4}>
+        Proficiencies
+        </Heading>
+        <SimpleGrid columns={[1,2,3]} spacing = {10}>
+            {proficiencies.map((skill, index) => (
+                <Box
+                key = {index} 
+                p={5}
+                shadow="md"
+                borderWidth="1px"
+                borderRadius="lg"
+                textAlign="center"
+                bg={useColorModeValue('white', 'gray.700')}
+                >
+                    <Text fontSize="xl" fontWeight="bold">
+                        {skill.name}
+                    </Text>
+                </Box>
+            ))}
+                </SimpleGrid>
+                </Container>
+                </Layout>
+)
+export default Proficiencies
