6ae36cf4ee45a9a6451aee8943afe736df41088d
JoMiras <jonamiraso@gmail.com>
2024-08-24 23:57:15 -0700

    Fixed deployment error

--- pages/interests.js
+++ pages/interests.js
@@ -2,7 +2,7 @@ import { Text, Container, Heading, SimpleGrid, Box, useColorModeValue } from '@c
 import Layout from '../components/layouts/article'
 
 const proficiencies = [
-    { name: 'HTML & CSS '},
+    { name: 'HTML & CSS ' },
     { name: 'JavaScript' },
     { name: 'SQL' },
     { name: 'Python'},
@@ -10,30 +10,42 @@ const proficiencies = [
     { name: 'C++'}
 ]
 
-const Proficiencies = () => (
-    <Layout title="Proficiencies"> 
-    <Container mt={{ base: '20px', md: '20px' }}>  
-        <Heading as="h3" fontSize={20} mb={4}>
-        Proficiencies
-        </Heading>
-        <SimpleGrid columns={[1,2,3]} spacing = {10}>
-            {proficiencies.map((skill, index) => (
-                <Box
-                key = {index} 
-                p={5}
-                shadow="md"
-                borderWidth="1px"
-                borderRadius="lg"
-                textAlign="center"
-                bg={useColorModeValue('white', 'gray.700')}
-                >
-                    <Text fontSize="xl" fontWeight="bold">
-                        {skill.name}
-                    </Text>
-                </Box>
-            ))}
+const Proficiencies = () => {
+    const bgColor = useColorModeValue('white', 'gray.700')
+    const borderColor = useColorModeValue('gray.200', 'gray.500')
+
+    return (
+        <Layout title="Proficiencies">
+            <Container mt={{ base: '20px', md: '20px' }}>
+                <Heading as="h3" fontSize={20} mb={4}>
+                    Proficiencies
+                </Heading>
+                <Text>!This page is still under construction!</Text>
+                <SimpleGrid columns={[1, 2, 3]} spacing={10}>
+                    {proficiencies.map((skill, index) => (
+                        <Box
+                            key={index}
+                            p={6}
+                            shadow="lg"
+                            borderWidth="2px"
+                            borderRadius="xl"
+                            textAlign="center"
+                            bg={bgColor}
+                            borderColor={borderColor}
+                            transition="transform 0.3s ease"
+                            _hover={{ transform: 'scale(1.05)' }}
+                        >
+                            <Box Position = "relative" zIndex={1}>
+                            <Text fontSize="xl" fontWeight="bold">
+                                {skill.name}
+                            </Text>
+                        </Box>
+                        </Box>
+                    ))}
                 </SimpleGrid>
-                </Container>
-                </Layout>
-)
+            </Container>
+        </Layout>
+    )
+}
+
 export default Proficiencies
